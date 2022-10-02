7ee7d629a4e54598c623b235ec1694cb16dd1e7f
alothmanmj <alothman.maj@gmail.com>
2022-10-02 14:37:27 +0300

    Created the skeleton of the section51 class

---
+++ Section51.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Section51{
+    
+
+    public static void main(String[] args) {
+        int[] arr = {3,7,1,5};
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
