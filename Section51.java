66f4975acebec4f08d24afc5f7bc15ef43c849fd
Abdulmjeed Abdullah Alothman <abdulmjeed.alothman@kfupm.edu.sa>
2022-10-02 15:29:37 +0300

    Add sout

--- Section51.java
+++ Section51.java
@@ -28,5 +28,7 @@ public class Section51{
         sort(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println("The END");
+        System.out.println("ABSJFSJ");
+        
     }
 }
