3d251bcd0e73db03cb11967deef2e6de6f8952ca
alothmanmj <alothman.maj@gmail.com>
2022-10-02 15:46:24 +0300

    all files

--- Section51.java
+++ Section51.java
@@ -25,7 +25,7 @@ public class Section51{
 
     public static void main(String[] args) {
         System.out.println("Start");
-        int[] arr = {3,7,1,5};
+        int[] arr = {3,0,4};
         sort(arr);
         System.out.println(Arrays.toString(arr));
         System.out.println("The END");
