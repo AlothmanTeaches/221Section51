a1f9e3a47318a12b57270818140fee128cdb9e1a
alothmanmj <alothman.maj@gmail.com>
2022-10-02 14:42:30 +0300

    Wrote the header of method sort

--- Section51.java
+++ Section51.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
 
+import javax.naming.ldap.SortControl;
+
 public class Section51{
-    
+
+    static void sort(int[] arr){
+        System.out.println("tbi later");
+    }
 
     public static void main(String[] args) {
         int[] arr = {3,7,1,5};
-
+        sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
